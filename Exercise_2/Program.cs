1ddb21c0f6adf7ed13887145c4b841c98b95367b
Nika <nikarasevas@gmail.com>
2022-09-28 21:52:29 +0300

    Функция: принимает число и выдаёт сумму цифр в числе

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма всех цифр числа {number} = {GetSum(number)}");
+
+int GetSum(int length)
+{
+    int sum = 0;
+    for(int temp = length; temp > 0; )
+    {
+        sum = sum + (temp % 10);
+        temp = temp / 10;
+    }
+    return sum;
+}
