186c7125f402db2e6326b43e453f0e1629963c7d
Nika <nikarasevas@gmail.com>
2022-09-28 21:47:09 +0300

    Функция: возводим A в степень B

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.WriteLine("Введите первое число: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Число {A} в степени {B} = {GetNumber(A, B)}");
+
+int GetNumber(int x, int y)
+{
+    return Convert.ToInt32(Math.Pow(x, y));
+}
